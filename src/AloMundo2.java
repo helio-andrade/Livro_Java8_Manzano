7c158ba5efbf0ac0c1ce945751135e110e17bdfe
Hélio Andrade <helio_andrade@hotmail.com.br>
2022-12-15 12:04:33 -0300

    Exercícios Cap. 04

---
+++ src/AloMundo2.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class AloMundo2 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, 
+				"Alô, Mundo!");
+		
+		System.exit(0);
+
+	}
+
+}
---
+++ src/C04Ex02.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
+public class C04Ex02 {
+
+	public static void main(String[] args) {
+		String nome="";
+		int idade=0;
+		float altura=0;
+		
+		DecimalFormat df = new DecimalFormat();
+		
+		System.out.println();
+		System.out.print("Digite seu nome ....: ");
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(System.in));
+			nome  = br.readLine();
+		} catch (Exception e) {
+			nome = "";
+		}
+		
+		System.out.print("Digite sua idade ....: ");
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(System.in));
+			idade  = Integer.parseInt(br.readLine());
+		} catch (Exception e) {
+			idade = 0;
+		}
+		
+		System.out.print("Digite sua altura ....: ");
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(System.in));
+			altura  = Float.parseFloat(br.readLine());
+		} catch (Exception e) {
+			altura = 0;
+		}
+		
+		System.out.println("Nome....: " + nome);
+		System.out.println("Idade ..: "	+ idade + " anos");
+		df.applyPattern("0.00");
+		System.out.println("Altura .: " + df.format(altura) + " m");
+	}
+
+}
