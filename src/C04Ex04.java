01af2a44e2a4c68728e05f7b41ce19552133d8d0
Hélio Andrade <helio_andrade@hotmail.com.br>
2022-12-17 11:04:23 -0300

    Update

---
+++ src/C04Ex04.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class C04Ex04 {
+
+	public static void main(String[] args) {
+		int a, b, r;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println();
+		System.out.println("A = ");
+		a = s.nextInt();
+		
+		System.out.println("B = ");
+		b = s.nextInt();
+		
+		r = a + b;
+		
+		System.out.println("\nR = " + a + " + " + b + " = " + r);
+
+	}
+
+}
