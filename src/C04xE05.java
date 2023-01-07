8b6c08f785bc26b14cbe4c5ff616f614d40aae92
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-01-07 09:19:21 -0300

    Create C04xE05.java

---
+++ src/C04xE05.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class C04xE05 {
+
+	public static void main(String[] args) {
+		int a, b, r;
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println();
+		
+		System.out.print("Digite o valor <A>: ");
+		a = s.nextInt();
+		
+		System.out.print("Digite o valor <B>: ");
+		b = s.nextInt();
+		
+		r = a + b;
+		System.out.println();
+		
+		if (r > 10)
+			System.out.println("Resultado ultrapassa o valor 10.");
+		else
+			System.out.println("Resultado nao ultrapassa o valor 10.");
+		System.out.println();
+	}
+
+}
