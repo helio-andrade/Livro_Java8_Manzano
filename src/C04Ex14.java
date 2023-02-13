6f4656e952d7596bb5e8219c0762582dcf431341
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-02-13 18:12:09 -0300

    Create C04Ex14.java

---
+++ src/C04Ex14.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class C04Ex14 {
+	public static void main(String[] args) {
+		
+		int dividendo, divisor, quociente;
+		
+		System.out.println("\nInforme um valor do dividendo: ");
+		try {
+			BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+			dividendo = Integer.parseInt(br.readLine());
+		} 
+		catch (Exception e) {
+			dividendo = 0;
+		}
+		
+		System.out.println("\nInforme um valor do divisor: ");
+		try {
+			BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+			divisor = Integer.parseInt(br.readLine());
+		} 
+		catch (Exception e) {
+			divisor = 0;
+		}
+		
+		try{
+			quociente = dividendo / divisor;
+			System.out.println("Resultado: " + quociente);
+		}
+		catch(ArithmeticException ae){
+			System.out.println("Erro de divisão por ZERO.");
+		}
+		
+	}
+}
