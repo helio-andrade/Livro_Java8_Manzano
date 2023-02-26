f8b02d72aaad3522f4ae775dfcfbd8409ba78086
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-02-26 18:37:16 -0300

    Atualização

---
+++ src/Comissao.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+class Comissao {
+    private static final int NUM_CORRETORES = 3;
+    private static final float COMISSAO_BAIXA = 0.07f;
+    private static final float COMISSAO_MEDIA = 0.095f;
+    private static final float COMISSAO_ALTA = 0.12f;
+
+    public static void main(String[] args) {
+        String relatorio = "Relatorio de Vendas";
+
+        for (int i = 1; i <= NUM_CORRETORES; i++) {
+            String nomeDoCorretor = JOptionPane.showInputDialog(
+                    "Digite o nome do CORRETOR n. " + i);
+
+            float valorDaVenda = Float.parseFloat(
+                    JOptionPane.showInputDialog("Valor da Venda (R$)"));
+
+            float comissao = calcularComissao(valorDaVenda);
+
+            relatorio = relatorio + "\n\nNome: " + nomeDoCorretor +
+                    "\nValor da Venda: R$ " + valorDaVenda +
+                    "\nComissao: R$ " + comissao;
+        }
+
+        JOptionPane.showMessageDialog(null, relatorio);
+    }
+
+    private static float calcularComissao(float valorDaVenda) {
+        if (valorDaVenda < 30000) {
+            return COMISSAO_BAIXA * valorDaVenda;
+        } else if (valorDaVenda <= 50000) {
+            return COMISSAO_MEDIA * valorDaVenda;
+        } else {
+            return COMISSAO_ALTA * valorDaVenda;
+        }
+    }
+}
---
+++ src/ConversaoDeTemperaturas.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ConversaoDeTemperaturas {
+	public static void main(String[] args) {
+		int opcao;
+		float temperatura;
+	    Scanner input = new Scanner(System.in);
+
+	    System.out.println("1 - Celsius -> Kelvin");
+	    System.out.println("2 - Celsius -> Fahrenheit");
+	    System.out.println("3 - Kelvin -> Celsius");
+	    System.out.println("4 - Kelvin -> Fahrenheit");
+	    System.out.println("5 - Fahrenheit -> Celsius");
+	    System.out.println("6 - Fahrenheit -> Kelvin");
+	    System.out.print("Escolha uma opção: ");
+	    opcao = input.nextInt();
+
+	    System.out.print("Digite a temperatura: ");
+	    temperatura = input.nextFloat();
+
+	    switch (opcao) {
+	        case 1:
+	            System.out.printf("%.2f°C = %.2f K", temperatura, temperatura + 273);
+	            break;
+	        case 2:
+	            System.out.printf("%.2f°C = %.2f°F", temperatura, (9.0/5.0)*temperatura + 32);
+	            break;
+	        case 3:
+	            System.out.printf("%.2f K = %.2f°C", temperatura, temperatura - 273);
+	            break;
+	        case 4:
+	            System.out.printf("%.2f K = %.2f°F", temperatura, (9.0/5.0)*(temperatura - 273) + 32);
+	            break;
+	        case 5:
+	            System.out.printf("%.2f°F = %.2f°C", temperatura, (5.0/9.0)*(temperatura - 32));
+	            break;
+	        case 6:
+	            System.out.printf("%.2f°F = %.2f K", temperatura, (5.0/9.0)*(temperatura - 32) + 273);
+	            break;
+	        default:
+	            System.out.println("Opção inválida.");
+	    }
+	}   
+}
