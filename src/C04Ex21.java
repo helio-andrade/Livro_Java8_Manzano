551a13c079fca112d79f4b00c6888b247fb03799
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-03-04 08:27:25 -0300

    Loops com testes no final

---
+++ src/C04Ex21.java
@@ -0,0 +1,12 @@
+public class C04Ex21 {
+	
+	public static void main(String[] args) {
+		int i = 1;
+		System.out.println();
+		
+		do {
+			System.out.println("Valor = " + i);
+			i++; // i = i + 1 ou i += 1
+		} while (i <= 5);
+	}
+}
