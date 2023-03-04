5d486e223f54e245211e7d109e93ba74652196be
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-03-04 06:57:23 -0300

    Create C04Ex18.java

---
+++ src/C04Ex18.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class C04Ex18 {
+	public static void main(String[] args) {
+		int i, resp;
+		
+		Scanner sc = new Scanner(System.in);
+	    
+		resp = 1;
+		while (resp == 1) {
+			System.out.println();
+			
+			i = 1;
+			while (i <= 5) {
+				System.out.println("Valor = " + i);
+				i++;
+			}
+			
+			System.out.println("Deseja continuar [1]SIM/[2]Não?");
+			resp = sc.nextInt();
+		}
+	}
+}
