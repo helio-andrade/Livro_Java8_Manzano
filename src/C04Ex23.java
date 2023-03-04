51bdc8e39c8b96ece30b5a7395fa4d8b129c39ab
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-03-04 08:35:21 -0300

    Create C04Ex23.java

---
+++ src/C04Ex23.java
@@ -0,0 +1,11 @@
+public class C04Ex23 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println();
+		
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Valor = " + i);
+		}
+	}
+}
