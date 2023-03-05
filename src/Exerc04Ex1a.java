fed974ae0a63a265c01ab7be6eb0ee9c967c9f6d
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-03-05 16:04:15 -0300

    Update

--- src/Exerc04Ex1a.java
+++ src/Exerc04Ex1a.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class Exerc04Ex1a {
+    public static void main(String[] args) {
+        float tempo, velocidade, distancia, litrosUsados;
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Tempo gasto (em horas): ");
+        tempo = input.nextFloat();
+
+        System.out.print("Velocidade média (em Km/h): ");
+        velocidade = input.nextFloat();
+
+        distancia = tempo * velocidade;
+        litrosUsados = distancia / 12;
+
+        System.out.printf("Velocidade média ......: %.2f Km/h\n", velocidade);
+        System.out.printf("Tempo gasto ...........: %.2f h\n", tempo);
+        System.out.printf("Distância percorrida ..: %.2f Km\n", distancia);
+        System.out.printf("Combustível gasto .....: %.2f litro(s)\n", litrosUsados);
+
+        input.nextLine(); // limpa o buffer do teclado
+        System.out.println("\nTecle ENTER para sair...");
+        input.nextLine(); // aguarda o ENTER
+    }
 }
---
+++ src/Exerc04Ex1b.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exerc04Ex1b {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Grau Celsius = ");
+        double celsius = sc.nextDouble();
+        
+        double fahrenheit = (9.0 * celsius + 160.0) / 5.0;
+        
+        System.out.printf("%6.2f °C = %6.2f °F\n", celsius, fahrenheit);
+        
+        System.out.println("\nTecle ENTER para sair...");
+        sc.nextLine();
+    }
+}
