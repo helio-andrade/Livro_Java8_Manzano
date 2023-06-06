d28e78c386071f82bdc94d69cd81eb1081995fcc
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-05 22:04:03 -0300

    Create C05Ex05.java

---
+++ src/capitulo5/C05Ex05.java
@@ -0,0 +1,10 @@
+package capitulo5;
+
+public class C05Ex05 {
+
+	public static void main(String[] args) {
+		String TEXTO = "Programação com Java";
+		
+		System.out.println(TEXTO.replace('a', 'e'));
+	}
+}
