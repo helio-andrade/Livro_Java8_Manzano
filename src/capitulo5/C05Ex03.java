d9f454c63bf597efee4edb18f659dde49145ff60
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-05 21:53:14 -0300

    Create C05Ex03.java

---
+++ src/capitulo5/C05Ex03.java
@@ -0,0 +1,11 @@
+package capitulo5;
+
+public class C05Ex03 {
+	public static void main(String[] args) {
+		String TEXTO1 = "Alô, ";
+		String TEXTO2 = "Mundo!";
+		
+		System.out.println(TEXTO1.concat(TEXTO2));
+		System.out.println(TEXTO1 + TEXTO2);
+	}
+}
