da3b1b672bfa3de448e2b0584d44bb1feb799be4
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-05 22:06:10 -0300

    Update C05Ex05.java

--- src/capitulo5/C05Ex05.java
+++ src/capitulo5/C05Ex05.java
@@ -4,7 +4,8 @@ public class C05Ex05 {
 
 	public static void main(String[] args) {
 		String TEXTO = "Programação com Java";
+		int TAMANHO = TEXTO.length();
 		
-		System.out.println(TEXTO.replace('a', 'e'));
+		System.out.println(TAMANHO);
 	}
 }
