3da02bca3ae8dfe40cad0cc8bcbf8665d575a4fa
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-05 22:21:29 -0300

    Update

---
+++ src/capitulo5/C05Ex08.java
@@ -0,0 +1,10 @@
+package capitulo5;
+
+public class C05Ex08 {
+	public static void main(String[] args) {
+		String	TEXTO = "Programação com Java";
+		
+		System.out.println(TEXTO.toUpperCase());
+		System.out.println(TEXTO.toLowerCase());
+	}
+}
---
+++ src/capitulo5/C05Ex09.java
@@ -0,0 +1,10 @@
+package capitulo5;
+
+public class C05Ex09 {
+	public static void main(String[] args) {
+		String TEXTO = " Programação com Java ";
+		
+		System.out.println("Com espaços: [" + TEXTO + "]");
+		System.out.println("Sem espaços: [" + TEXTO.trim() + "]");
+	}
+}
