2f25152a0ea2dde5106f824b9a5691cdcdb0468d
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-05 22:15:32 -0300

    Create C05Ex07.java

---
+++ src/capitulo5/C05Ex07.java
@@ -0,0 +1,11 @@
+package capitulo5;
+
+public class C05Ex07 {
+	public static void main(String[] args) {
+		String	TEXTO = "Computador";
+		
+		System.out.println(TEXTO.substring(0, 3));
+		System.out.println(TEXTO.substring(3, 7));
+		System.out.println(TEXTO.substring(7, 10));
+	}
+}
