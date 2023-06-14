c6a06b0b68a53c6115c52e8f0d1580baba5fd101
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-14 20:32:36 -0300

    Update C05Ex10.java

--- src/capitulo5/C05Ex10.java
+++ src/capitulo5/C05Ex10.java
@@ -1,25 +1,43 @@
 package capitulo5;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class C05Ex10 {
-	public static void main(String[] args) {
-		int idade;
-		float altura;
-		
-		Scanner s = new Scanner(System.in);
-		
-		System.out.println("Idade ....: ");
-		idade = s.nextInt();
-		
-		System.out.println("Altura ...: ");
-		altura = s.nextFloat();
-		
-		if (idade <= 1)
-			System.out.println("Idade " + String.valueOf(idade) + " ano.");
-		else
-			System.out.println("Idade " + String.valueOf(idade) + " anos.");
-		
-		System.out.println("Altura " + String.valueOf(altura) + " cm.");
-	}
+    public static void main(String[] args) {
+        int idade;
+        float altura;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Idade ....: ");
+        try {
+            idade = s.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida para idade.");
+            s.close();
+            return;
+        }
+
+        System.out.print("Altura ...: ");
+        try {
+            altura = s.nextFloat();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida para altura.");
+            s.close();
+            return;
+        }
+
+        String idadeString = String.valueOf(idade);
+        String alturaString = String.valueOf(altura);
+
+        if (idade <= 1)
+            System.out.println("Idade " + idadeString + " ano.");
+        else
+            System.out.println("Idade " + idadeString + " anos.");
+
+        System.out.println("Altura " + alturaString + " cm.");
+
+        s.close();
+    }
 }
