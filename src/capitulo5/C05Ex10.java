a86583b62ad3a480535ae5d4079219a9e66137e1
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-14 20:20:45 -0300

    Create C05Ex10.java

---
+++ src/capitulo5/C05Ex10.java
@@ -0,0 +1,25 @@
+package capitulo5;
+
+import java.util.Scanner;
+
+public class C05Ex10 {
+	public static void main(String[] args) {
+		int idade;
+		float altura;
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Idade ....: ");
+		idade = s.nextInt();
+		
+		System.out.println("Altura ...: ");
+		altura = s.nextFloat();
+		
+		if (idade <= 1)
+			System.out.println("Idade " + String.valueOf(idade) + " ano.");
+		else
+			System.out.println("Idade " + String.valueOf(idade) + " anos.");
+		
+		System.out.println("Altura " + String.valueOf(altura) + " cm.");
+	}
+}
