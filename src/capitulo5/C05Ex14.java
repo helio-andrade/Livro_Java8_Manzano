df9b4f1ae6db4d2a78b17f53417f76dc646c0990
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-18 10:42:23 -0300

    Create C05Ex14.java

---
+++ src/capitulo5/C05Ex14.java
@@ -0,0 +1,13 @@
+package capitulo5;
+
+public class C05Ex14 {
+	public static void main(String[] args) {
+		double valor = 23.78976;
+		String str;
+		
+		str = Double.toString(valor);
+		
+		System.out.println("\nValor = " + str);
+		
+	}
+}
