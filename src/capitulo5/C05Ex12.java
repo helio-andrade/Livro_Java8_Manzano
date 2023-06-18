f2cb04f7bcbe86ec9f5e571f63b5ca5b1ef177a3
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-18 10:02:18 -0300

    Update C05Ex12.java

--- src/capitulo5/C05Ex12.java
+++ src/capitulo5/C05Ex12.java
@@ -12,7 +12,17 @@ public class C05Ex12 {
 		
 		System.out.println("Digite um código ASCII: ");
 		ascii = s.nextInt();
+		
 		//str = new Character(char(ascii)).toString();
+		
+		/*
+		 *  A classe Character possui um método estático toString(char) 
+		 *  que pode ser usado diretamente para converter o caractere 
+		 *  em uma string, sem a necessidade de criar um objeto Character 
+		 *  e chamá-lo.
+		 *   
+		 * */
+		
 		str = Character.toString((char) ascii);
 		
 		System.out.println(ascii + " corresponde ao caractere " + str);
