ce71b469b1c8b66a2a2448ba767396da665cb11a
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-18 10:28:27 -0300

    Create C05Ex13.java

---
+++ src/capitulo5/C05Ex13.java
@@ -0,0 +1,21 @@
+package capitulo5;
+
+public class C05Ex13 {
+	public static void main(String[] args) {
+		String str = "ESTUDO DE JAVA";
+		int i, seqASCII;
+		char caractere;
+		
+		for(i = 0; i < str.length(); i++) {
+			caractere = str.charAt(i);
+			System.out.println(" " + caractere + " ");
+		}
+		
+		for(i = 0; i < str.length(); i++) {
+			caractere = str.charAt(i);
+			seqASCII = (int)caractere;
+			System.out.println(" " + seqASCII + " ");
+		}		
+		
+	}
+}
