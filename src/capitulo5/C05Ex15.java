f2d641b3440a067d7585e6fde114b6b42feb9add
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-22 11:16:06 -0300

    Create C05Ex15.java

---
+++ src/capitulo5/C05Ex15.java
@@ -0,0 +1,15 @@
+package capitulo5;
+
+public class C05Ex15 {
+
+	public static void main(String[] args) {
+		float valor = 35;
+		String str;
+		
+		str = Float.toString(valor);
+		
+		System.out.println("Valor = " + str);
+
+	}
+
+}
