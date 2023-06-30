396c65614fc9119ceda9e2a2977211a933b8b697
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-30 14:17:07 -0300

    Create C05Ex30.java

---
+++ src/capitulo5/C05Ex30.java
@@ -0,0 +1,13 @@
+package capitulo5;
+
+public class C05Ex30 {
+	public static void main(String[] args) {
+		int inteiro = 0;
+		boolean logico;
+		
+		logico = inteiro != 0;
+		
+		System.out.println("\nValor verdadeiro de INTEIRO ..: " + inteiro);
+		System.out.println("\nValor verdadeiro de LOGICO ...: " + logico);
+	}
+}
