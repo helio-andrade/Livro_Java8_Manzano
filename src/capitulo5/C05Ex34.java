989055d5684b5abdd1095f779018603fafe13340
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-06-30 22:15:56 -0300

    Update C05Ex34.java

--- src/capitulo5/C05Ex34.java
+++ src/capitulo5/C05Ex34.java
@@ -2,21 +2,17 @@ package capitulo5;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import javax.swing.JOptionPane;
 
 public class C05Ex34 {
     public static void main(String[] args) {
-
         LocalDate dataSistema = LocalDate.of(1981, 2, 8); // Data específica: 08/02/1981
         DayOfWeek diaSemana = dataSistema.getDayOfWeek();
-        int diaSemanaInt = dataSistema.getDayOfWeek().getValue();
+        int diaSemanaInt = diaSemana.getValue();
         int diasAno = dataSistema.getDayOfYear();
 
-        String mensagem = "Data específica: " + dataSistema +
-                "\nDia da semana (extenso): " + diaSemana +
-                "\nDia da semana (numero): " + diaSemanaInt +
-                "\nNúmero de dias no ano: " + diasAno;
-
-        JOptionPane.showMessageDialog(null, mensagem, "Informações de Data", JOptionPane.INFORMATION_MESSAGE);
+        System.out.println("Data específica .........: " + dataSistema);
+        System.out.println("Dia da semana (extenso) .: " + diaSemana);
+        System.out.println("Dia da semana (numero) ..: " + diaSemanaInt);
+        System.out.println("Numero de dias no ano ...: " + diasAno);
     }
 }
