4a7cfbff58a5461c743a9724205609a14947d6c2
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-08-13 12:08:59 -0300

    Update
    Iniciando as atividades do Capítulo 6

---
+++ src/capitulo6/C06Ex01.java
@@ -0,0 +1,13 @@
+package capitulo6;
+
+public class C06Ex01 {
+	public static void main(String[] args) {
+		System.out.println("Execução do Programa Principal - 1");
+		subRotina();
+		System.out.println("Execução do Programa Principal - 2");
+	}
+	
+	public static void subRotina() {
+		System.out.println("Execução da sub-rotina");
+	}
+}
---
+++ src/capitulo6/C08Ex02.java
@@ -0,0 +1,147 @@
+package capitulo6;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class C08Ex02 {
+    private static final Scanner input = new Scanner(System.in);
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+
+    public static void main(String[] args) {
+        byte opcao;
+
+        String menu = "\n"
+                + "--------------------------\n"
+                + "PROGRAMA CALCULADORA\n"
+                + "Menu Principal\n"
+                + "--------------------------\n"
+                + "[1] ............. Adicao\n"
+                + "[2] .......... Subtracao\n"
+                + "[3] ...... Multiplicacao\n"
+                + "[4] ............ Divisao\n"
+                + "[5] ........... Encerrar\n"
+                + "\nEscolha uma opcao: ";
+
+        do {
+            clearScreen();
+            System.out.println(menu);
+            opcao = input.nextByte();
+
+            switch (opcao) {
+                case 1:
+                    rotAdicao();
+                    break;
+                case 2:
+                    rotSubtracao();
+                    break;
+                case 3:
+                    rotMultiplicacao();
+                    break;
+                case 4:
+                    rotDivisao();
+                    break;
+                case 5:
+                    System.out.println("Encerrando o programa.");
+                    break;
+                default:
+                    System.out.println("Opcao invalida.");
+                    break;
+            }
+
+            pausa();
+        } while (opcao != 5);
+
+        input.close();
+    }
+
+    private static void rotAdicao() {
+        double r, a, b;
+
+        System.out.println("<<< ROTINA DE ADICAO >>>\n");
+
+        System.out.print("a = ");
+        a = input.nextDouble();
+        System.out.print("b = ");
+        b = input.nextDouble();
+
+        r = a + b;
+        System.out.println(a + " + " + b + " = " + df.format(r));
+    }
+
+    private static void rotSubtracao() {
+        double r, a, b;
+
+        System.out.println("<<< ROTINA DE SUBTRACAO >>>\n");
+
+        System.out.print("a = ");
+        a = input.nextDouble();
+        System.out.print("b = ");
+        b = input.nextDouble();
+
+        r = a - b;
+        System.out.println(a + " - " + b + " = " + df.format(r));
+    }
+
+    private static void rotMultiplicacao() {
+        double r, a, b;
+
+        System.out.println("<<< ROTINA DE MULTIPLICACAO >>>\n");
+
+        System.out.print("a = ");
+        a = input.nextDouble();
+        System.out.print("b = ");
+        b = input.nextDouble();
+
+        r = a * b;
+        System.out.println(a + " * " + b + " = " + df.format(r));
+    }
+
+    private static void rotDivisao() {
+        double r, a, b;
+
+        System.out.println("<<< ROTINA DE DIVISAO >>>\n");
+
+        System.out.print("a = ");
+        a = input.nextDouble();
+        System.out.print("b = ");
+        b = input.nextDouble();
+
+        if (b != 0) {
+            r = a / b;
+            System.out.println(a + " / " + b + " = " + df.format(r));
+        } else {
+            System.out.println("Divisao por zero nao e permitida.");
+        }
+    }
+
+    private static void clearScreen() {
+        String os = System.getProperty("os.name").toLowerCase();
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        try {
+            if (os.contains("win")) {
+                processBuilder.command("cmd", "/c", "cls");
+            } else {
+                processBuilder.command("clear");
+            }
+
+            processBuilder.inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void pausa() {
+        System.out.print("\nTecle <ENTER> para continuar...");
+        try {
+            br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
