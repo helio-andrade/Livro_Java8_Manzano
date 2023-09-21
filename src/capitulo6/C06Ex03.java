b50d0400fd4beabe010e09baa816d861df6e3284
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-09-20 23:01:26 -0300

    Create C06Ex03.java

---
+++ src/capitulo6/C06Ex03.java
@@ -0,0 +1,24 @@
+package capitulo6;
+
+import java.util.Scanner;
+
+public class C06Ex03 {
+	public static void main(String[] args) {
+		byte x;
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("Digite um valor inteiro (de 0 a 25): ");
+		x = s.nextByte();
+		fatorial(x);
+	}
+	
+	public static void fatorial(byte n) {
+		long fat = 1;
+		if (n == 0)
+			fat = 0;
+		else
+			for (int i = 1; i <= n; i++)
+				fat *= i;
+		System.out.println(n + "! = " + fat);
+	}
+}
