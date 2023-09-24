5b0abcdd16e6fe5ccffc2000b20de6a414807e7d
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-09-23 21:17:43 -0300

    Create C07Ex01.java

---
+++ src/capitulo7/C07Ex01.java
@@ -0,0 +1,20 @@
+package capitulo7;
+
+import java.util.Scanner;
+
+public class C07Ex01 {
+	public static void main(String[] args) {
+		class Aluno{
+			String nome;
+			float media;
+		}
+		
+		Aluno aluno = new Aluno();
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		input.close();
+		
+	}
+}
