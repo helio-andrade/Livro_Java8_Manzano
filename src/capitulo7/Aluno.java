ce35e1e37588ad0eae814b61c34de49500af51f2
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-09-23 23:17:45 -0300

    Update

--- src/capitulo7/Aluno.java
+++ src/capitulo7/Aluno.java
@@ -1,15 +1,35 @@
 package capitulo7;
 
 public class Aluno {
-	String nome;
-	float media;
-	
-	public static boolean situacaoAluno(float valorMedia, double mediaAprovacao) {
-		boolean situacao = true;
-		
-		if (valorMedia < mediaAprovacao)
-			situacao = false;
-		
-		return(situacao);
-	}
+    private String nome;
+    private float media;
+
+    public Aluno() {
+        // Construtor vazio
+    }
+
+    public Aluno(String nome, float media) {
+        this.nome = nome;
+        this.media = media;
+    }
+
+    public boolean situacaoAluno(double mediaAprovacao) {
+        return media >= mediaAprovacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getMedia() {
+        return media;
+    }
+
+    public void setMedia(float media) {
+        this.media = media;
+    }
 }
--- src/capitulo7/C07Ex03.java
+++ src/capitulo7/C07Ex03.java
@@ -2,21 +2,23 @@ package capitulo7;
 
 import java.util.Scanner;
 
-public class C07Ex02 {
+public class C07Ex03 {
     public static void main(String[] args) {
-        Aluno aluno = new Aluno();
         Scanner input = new Scanner(System.in);
 
         System.out.print("Nome ...: ");
         String nome = input.nextLine();
-        aluno.setNome(nome);
 
-        System.out.print("Media ..: ");
+        System.out.print("Média ..: ");
         float media = input.nextFloat();
-        aluno.setMedia(media);
+
+        Aluno aluno = new Aluno(nome, media);
+
+        String situacao = aluno.situacaoAluno(5.0) ? "APROVADO!" : "REPROVADO!";
+        System.out.println(situacao);
 
         System.out.println("Nome: " + aluno.getNome());
-        System.out.println("Media: " + aluno.getMedia());
+        System.out.println("Média: " + aluno.getMedia());
 
         input.close();
     }
