2476684262af3d35364657422d8d519b31fad694
Hélio Andrade <helio_andrade@hotmail.com.br>
2023-09-23 22:59:37 -0300

    Update Aluno.java

--- src/capitulo7/Aluno.java
+++ src/capitulo7/Aluno.java
@@ -3,4 +3,13 @@ package capitulo7;
 public class Aluno {
 	String nome;
 	float media;
+	
+	public static boolean situacaoAluno(float valorMedia, double mediaAprovacao) {
+		boolean situacao = true;
+		
+		if (valorMedia < mediaAprovacao)
+			situacao = false;
+		
+		return(situacao);
+	}
 }
